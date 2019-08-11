bb4b25bf5691ae3cfdd5c3d8c5c144f842e25a70
Henry Nguyen <nguyenhai1591@gmail.com>
2019-08-11 15:22:17 +1000

    change db url

--- app.py
+++ app.py
@@ -47,7 +47,7 @@ Session(app)
 
 # Set up database (new way)
 # Commented out to try deploying app
-app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres:legolas136@localhost/postgres"
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgres://slrdxhcahegldy:d08aa992a6d5d714b50c66dece9cca10c9d4b61d01ac705fbb9d205fe857bac7@ec2-50-19-222-129.compute-1.amazonaws.com:5432/dam82ofh6k9khf"
 # app.config['SQLALCHEMY_DATABASE_URI'] = "postgres://fkrrwcrbpazwzy:d91e42e15d71e1faab561ad19c006f22090503a5db973c08e4d40dc5271a5417@ec2-54-243-208-234.compute-1.amazonaws.com:5432/daorepv9mko282"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
