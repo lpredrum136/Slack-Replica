df7d58e15557fef3ccfc4fca84a8fc16a7d87255
Henry Nguyen <nguyenhai1591@gmail.com>
2019-08-11 17:12:49 +1000

    fix db (try)

--- create.py
+++ create.py
@@ -4,8 +4,8 @@ from models import *
 app = Flask(__name__)
 
 # commented out to try deploying app
-app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres:legolas136@localhost/postgres"
-# app.config['SQLALCHEMY_DATABASE_URI'] = "postgres://fkrrwcrbpazwzy:d91e42e15d71e1faab561ad19c006f22090503a5db973c08e4d40dc5271a5417@ec2-54-243-208-234.compute-1.amazonaws.com:5432/daorepv9mko282"
+# app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres:legolas136@localhost/postgres"
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgres://slrdxhcahegldy:d08aa992a6d5d714b50c66dece9cca10c9d4b61d01ac705fbb9d205fe857bac7@ec2-50-19-222-129.compute-1.amazonaws.com:5432/dam82ofh6k9khf"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db.init_app(app)
